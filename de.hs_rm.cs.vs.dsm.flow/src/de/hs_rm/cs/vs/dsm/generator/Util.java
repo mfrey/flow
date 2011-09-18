afa9106e796c1a34b4e9c942dbcc527f313cab01
Michael Frey <mail@mfrey.net>
2011-09-18 14:41:27 +0200

    Added barrier method to the Util class

--- de.hs_rm.cs.vs.dsm.flow/src/de/hs_rm/cs/vs/dsm/generator/Util.java
+++ de.hs_rm.cs.vs.dsm.flow/src/de/hs_rm/cs/vs/dsm/generator/Util.java
@@ -6,6 +6,10 @@ import org.eclipse.emf.common.util.EList;
 
 import de.hs_rm.cs.vs.dsm.flow.StreamDefinition;
 
+/**
+ *  
+ * 
+ */
 public class Util {
 	private static final Util mInstance = new Util();
 	
@@ -30,8 +34,42 @@ public class Util {
 				pIn + ", \"" + pIndirection + "\");\n";
 	}
 	
+	public String createBarrier(final String pStream, final String pWindowType, final String pValue){
+		// The result string
+		String result = "";
+		// If the window type is of type time
+		if(pWindowType.equals("time")){
+			result += Util.getInstance().createParameter(pStream, "step_size_num", pValue);
+			result += Util.getInstance().createParameter(pStream, "window_type", "time");
+		// If the window type is of type elements
+		}else if(pWindowType.equals("elements")){
+			// TODO
+		// If the window type is of type now
+		}else if(pWindowType.equals("now")){
+			// TODO
+		// If the window type is a marker, which probably requires a seperate method
+		}else{
+			//TODO
+		}
+		return result;
+	}
+	
+	/**
+	 * The method creates a parameter for an operator. Every parameter of an operator consists of
+	 * an key/value pair. For example
+	 * 
+	 *	avg:set_parameter("window_type", "num");
+	 * 
+	 * where for a stream 'avg' a parameter 'window_type' is set to 'num'
+	 * 
+	 * @return A string which contains a set_parameter operation
+	 */
+	public String createParameter(final String pStream, final String pKey, final String pValue){
+		return pStream + ":set_parameter(" + "\"" + pKey + "\"" + ", " + "\"" + pValue + "\"" + ");\n";
+	}
+	
 	public String connectOperator(final ArrayList<String> pIn, final String pIndirection, final ArrayList<String> pOut, final String pOutDirection){
-		return "";
+		return "TODO \n";
 	}
 	
 	public String connectOperator(final String pIn, final String pIndirection, final ArrayList<String> pOut, final String pOutDirection){
