34ae492c6a8d450aed678607333a7d6b8208019a
Michael Frey <mail@mfrey.net>
2011-09-22 17:29:19 +0200

    Added a 'simple' test (otherwise the build fails)

---
+++ de.hs_rm.cs.vs.dsm.flow.tests/src/de/hs_rm/cs/vs/dsm/flow/tests/ParserGeneratorTest.java
@@ -0,0 +1,20 @@
+package de.hs_rm.cs.vs.dsm.flow.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserGeneratorTest {
+
+	@Before
+	public void initialize(){
+		
+	}
+	
+	@Test
+	public void simpleTest(){
+		// Just for the sake of the build process, will add other unit tests later
+		assertTrue(true);
+	}
+}
