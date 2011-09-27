892de49b2e11cfeebacb3228fd669bf5c6d9c0fb
Michael Frey <mail@mfrey.net>
2011-09-27 23:12:53 +0200

    Added unit test stubs

---
+++ de.hs_rm.cs.vs.dsm.flow.tests/src/de/hs_rm/cs/vs/dsm/flow/tests/SystemOperatorGeneratorTest.java
@@ -0,0 +1,25 @@
+package de.hs_rm.cs.vs.dsm.flow.tests;
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SystemOperatorGeneratorTest {
+	@Before
+	public void initialize(){
+		
+	}
+	
+	@Test
+	public void testOutputOperatorGenerator(){
+
+	}
+	
+	@Test
+	public void testInputOperatorGenerator(){
+
+	}
+}
